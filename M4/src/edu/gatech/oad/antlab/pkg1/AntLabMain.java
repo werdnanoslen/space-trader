6f0159a5244e57693919a84b726a5c6deb0eb77b
Andrew Nelson <werdnanoslen@gmail.com>
2012-09-30 18:30:23 -0400

    Person1 is Andrew Nelson

--- M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -59,8 +59,8 @@ import edu.gatech.oad.antlab.pkg2.*;
           + ant22.getMessage() + ant31.getMessage();
 		  //Person1 replace P1 with your name
 		  //and ggg000x with your gt num
-		  Person1 p1 = new Person1("P1");
-		  toPrint += p1.toString("ggg001x");
+		  Person1 p1 = new Person1("Andrew Nelson");
+		  toPrint += p1.toString("anelson35");
 		  //Person2 replace P2 with your name
 		  //and ggg000x with your gt num
 		  Person2 p2 = new Person2("P2");
