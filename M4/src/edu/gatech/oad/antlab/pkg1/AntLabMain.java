ce4ce9cdc131507f9c834b426e184a38b995d265
DLi42 <davelid136@gmail.com>
2012-10-01 21:51:02 -0400

    Added Edited AntLabMain Person 4

--- M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -71,8 +71,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p3.toString("ggg003x");
           //Person4 replace P4 with your name
           //and ggg000x with your gt num
-          Person4 p4 = new Person4("P4");
-          toPrint += p4.toString("ggg004x");
+          Person4 p4 = new Person4("David Li");
+          toPrint += p4.toString("dli42");
 		  //Person5 replace P5 with your name
           //and ggg000x with your gt num
           Person5 p5 = new Person5("P5");
