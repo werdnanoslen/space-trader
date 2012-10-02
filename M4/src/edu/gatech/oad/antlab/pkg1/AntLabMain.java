e6d825ca7c0f0e4697d7bd6af2c53b14104f31cc
Nikhil Dev Deshmudre <nikhil.dev91@gmail.com>
2012-10-01 23:45:15 -0400

    Completed M4

--- M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -75,8 +75,8 @@ import edu.gatech.oad.antlab.pkg2.*;
           toPrint += p4.toString("dli42");
 		  //Person5 replace P5 with your name
           //and ggg000x with your gt num
-          Person5 p5 = new Person5("P5");
-          toPrint += p5.toString("ggg005x");
+          Person5 p5 = new Person5("Nikhil Dev Deshmudre");
+          toPrint += p5.toString("ndeshmudre3");
         System.out.println(toPrint);
         
         
