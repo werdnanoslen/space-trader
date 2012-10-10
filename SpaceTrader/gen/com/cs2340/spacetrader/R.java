b601216d6166b1f47edfe37264a200fc2fd41bc8
alex <alexander.trimm@gmail.com>
2012-10-10 15:25:26 -0400

    Added functional but ugly Setup Screen

--- SpaceTrader/gen/com/cs2340/spacetrader/R.java
+++ SpaceTrader/gen/com/cs2340/spacetrader/R.java
@@ -15,9 +15,24 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button_loadGame=0x7f070000;
-        public static final int button_newGame=0x7f070001;
-        public static final int menu_settings=0x7f070002;
+        public static final int button1=0x7f07000e;
+        public static final int button_loadGame=0x7f07000f;
+        public static final int button_newGame=0x7f070010;
+        public static final int editText1=0x7f070000;
+        public static final int editText2=0x7f070001;
+        public static final int editText3=0x7f070002;
+        public static final int editText4=0x7f070003;
+        public static final int editText5=0x7f070004;
+        public static final int editText6=0x7f070009;
+        public static final int menu_settings=0x7f070011;
+        public static final int numberPicker1=0x7f070005;
+        public static final int numberPicker2=0x7f070006;
+        public static final int numberPicker3=0x7f070007;
+        public static final int numberPicker4=0x7f070008;
+        public static final int radio0=0x7f07000b;
+        public static final int radio1=0x7f07000c;
+        public static final int radio2=0x7f07000d;
+        public static final int radioGroup1=0x7f07000a;
     }
     public static final class layout {
         public static final int activity_game_setup=0x7f030000;
@@ -28,6 +43,7 @@ public final class R {
         public static final int activity_launcher=0x7f060001;
     }
     public static final class string {
+        public static final int allocation_error=0x7f040008;
         public static final int app_name=0x7f040000;
         public static final int button_loadGame=0x7f040005;
         public static final int button_newGame=0x7f040004;
