b435b29427469cfdad526db1dd9a611cb1e43c2a
DLi42 <davelid136@gmail.com>
2012-10-31 00:59:49 -0400

    linked space, planet, and trade views

---
+++ SpaceTrader/src/com/cs2340/spacetrader/PlanetView.java
@@ -0,0 +1,42 @@
+package com.cs2340.spacetrader;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.support.v4.app.NavUtils;
+
+public class PlanetView extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planet);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_planet_view, menu);
+        return true;
+    }
+
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void startTradeView(View view){
+    	Intent intent = new Intent(this, TradeView.class);
+    	startActivity(intent);
+    }
+
+}
