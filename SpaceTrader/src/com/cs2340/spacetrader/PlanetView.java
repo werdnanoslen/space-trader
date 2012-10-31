0d60dde2e7f86b329e00d18671784ad806557e40
DLi42 <davelid136@gmail.com>
2012-10-31 01:23:08 -0400

    fixed an error in PlanetView

--- SpaceTrader/src/com/cs2340/spacetrader/PlanetView.java
+++ SpaceTrader/src/com/cs2340/spacetrader/PlanetView.java
@@ -16,23 +16,6 @@ public class PlanetView extends Activity {
         setContentView(R.layout.activity_planet);
         getActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_planet_view, menu);
-        return true;
-    }
-
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
     
     public void startTradeView(View view){
     	Intent intent = new Intent(this, TradeView.class);
