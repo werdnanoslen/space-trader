594dd5e1cd2a2e30090a0bc13e1d556e2757b6eb
alex <alexander.trimm@gmail.com>
2012-11-13 00:30:16 -0500

    Additional code addition for facilitating weapon and shield upgrades

--- SpaceTrader/src/com/cs2340/spacetrader/Ship.java
+++ SpaceTrader/src/com/cs2340/spacetrader/Ship.java
@@ -8,7 +8,8 @@ public class Ship implements Serializable {
 	private String planetName;
 	private int fuel;
 	private int fuelCapacity;
-	
+	private int weaponLevel;
+	private int shieldLevel;
 
 	public Ship(int gold, int nSlots)
 	{
@@ -17,6 +18,8 @@ public class Ship implements Serializable {
 		//TODO think of a centralized place to put all these kinds of numbers.
 		this.fuel = 100;
 		this.fuelCapacity = 100;
+		this.weaponLevel=1;
+		this.shieldLevel=1;
 	}
 
 	public ShipInventory getInventory()
@@ -64,9 +67,100 @@ public class Ship implements Serializable {
 			this.fuel = newFuel;
 	}
 	
+	
+	public void upgradeWeapons(){
+		//TODO - check if there are any upgrades level
+		weaponLevel = weaponLevel +1;
+		inventory.deltaMoney(-(wepUpgradeCost()));
+	}
+	
+	public String wepUpgradeName() {
+		return Weapons.nextWeapon(weaponLevel).getName();
+	}
+	
+	public int wepUpgradeCost(){
+		return Weapons.nextWeapon(weaponLevel).getPrice();
+	}
+	
+	public void upgradeShields(){
+		//TODO - check if there are any upgrades level
+		shieldLevel = shieldLevel +1;
+		inventory.deltaMoney(-(shieldUpgradeCost()));
+	}
+	
+	public String shieldUpgradeName() {
+		return Shields.nextShield(shieldLevel).getName();
+	}
+	
+	public int shieldUpgradeCost(){
+		return Shields.nextShield(shieldLevel).getPrice();
+	}
+	
 	private int fuelMetric(int deltaX, int deltaY)
 	{
 		int squaredCost = (int) (Math.pow((int)deltaX,2)+ Math.pow((int)deltaY, 2));
 		return (int) Math.round(Math.sqrt(squaredCost));
+	}
+
+	enum Weapons {
+		Blaster("Blaster Pistol",1),
+		Gat("Gattling Guns",3),
+		Photon("Photon Torpedos",5),
+		Antimatter("Antimatter",11);
+		
+		 final String name;
+		 final int power;
+		 
+		 Weapons(String name,final int power){
+			 this.name =name;
+			 this.power=power;
+		 }
+		 
+		 String getName() {
+			 return name;
+		 }
+		 
+		 int getPower() {
+			 return power;
+		 }
+		 
+		 int getPrice() {
+			 return (int) Math.pow(power, 2)*500;
+		 }
+		 
+		 static Weapons nextWeapon(int WeaponLevel) {
+		 	return Weapons.values()[WeaponLevel];
+		 }
+	}
+	
+	enum Shields {
+		Trashcans("Old Trashcans",1),
+		Steel("Steel Plates",4),
+		Force("Force Field",5),
+		Temporal("Temporal Evasion",20);
+		
+		 final String name;
+		 final int power;
+		 
+		 Shields(String name,final int power){
+			 this.name =name;
+			 this.power=power;
+		 }
+		 
+		 String getName() {
+			 return name;
+		 }
+		 
+		 int getPower() {
+			 return power;
+		 }
+		 
+		 int getPrice() {
+			 return (int) Math.pow(power, 2)*500;
+		 }
+		 
+		 static Shields nextShield(int ShieldLevel) {
+		 	return Shields.values()[ShieldLevel];
+		 }
 	}
 }
