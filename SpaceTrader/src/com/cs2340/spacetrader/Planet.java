7c583e8875c70d538bbdbe8d3f9fc7814d7a9bee
Nikhil Dev Deshmudre <nikhil.dev91@gmail.com>
2012-11-21 09:37:19 -0500

    fixed most audit issues for Planet.java

--- SpaceTrader/src/com/cs2340/spacetrader/Planet.java
+++ SpaceTrader/src/com/cs2340/spacetrader/Planet.java
@@ -1,32 +1,42 @@
+// $codepro.audit.disable booleanMethodNamingConvention
 package com.cs2340.spacetrader;
 
 import java.io.Serializable;
 
-public class Planet implements Serializable
-{
-	/**2-D coordinate of planet **/
-	private int[] coordinate;
-	
-	/**Whether market is currently being traded with **/
-	private boolean marketBusy; // indicates whether a ship is presently visiting a market 
-	
-	/**Inventory the planet has available for trade **/
+
+/**
+ * This class encapsulates the concept of a Planet and stores vital 
+ * information normally associated with a planet. It mainly has getters 
+ * and setters to access the information.
+ * @author The Droids You Are Looking For
+ * @version 1.0
+ */
+public class Planet implements Serializable {
+	/** 2-D coordinate of planet **/
+	private final int[] coordinate;
+
+	/** Whether market is currently being traded with **/
+	private boolean marketBusy; // indicates whether a ship is presently
+								// visiting a market
+
+	/** Inventory the planet has available for trade **/
 	private PlanetInventory inventory;
-	
-	/**Contract planet has available **/
-	private Contract contract;
-	
-	/**Name of the planet **/
-	private String name;
-	
-	/**Technology level of the planet **/
+
+	/** Contract planet has available **/
+	private Contract contract; // $codepro.audit.disable variableShouldBeFinal
+
+	/** Name of the planet **/
+	private final String name;
+
+	/** Technology level of the planet **/
 	private String techLevel;
-	
-	/**numeric tech level for calculating good prices **/
-	private int nTechLevel;
+
+	/** numeric tech level for calculating good prices **/
+	private final int nTechLevel;
 
 	/**
 	 * Constructor for Planet class
+	 * 
 	 * @param name
 	 * @param coord
 	 * @param inventory
@@ -34,59 +44,60 @@ public class Planet implements Serializable
 	 * @param nTechLevel
 	 */
 	public Planet(String name, int[] coord, PlanetInventory inventory,
-			Contract contract, int nTechLevel)
-	{
+			Contract contract, int nTechLevel) {
 		this.marketBusy = false;
 		this.coordinate = coord.clone();
 		this.inventory = inventory;
 		this.name = name;
 		this.contract = contract;
 		this.nTechLevel = nTechLevel;
-		switch (nTechLevel) 
-		{
-			case 0:
-				techLevel = "Pre-Agriculture";
-				break;
-			case 1:
-				techLevel = "Agriculture";
-				break;
-			case 2:
-				techLevel = "Medieval";
-				break;
-			case 3:
-				techLevel = "Renaissance";
-				break;
-			case 4:
-				techLevel = "Early Industrial";
-				break;
-			case 5:
-				techLevel = "Industrial";
-				break;
-			case 6:
-				techLevel = "Post-Industrial";
-				break;
-			case 7:
-				techLevel = "Hi-Tech";
-				break;
-			default:
-				techLevel = "undefined";
+		switch (nTechLevel) {
+		case 0:
+			techLevel = "Pre-Agriculture";
+			break;
+		case 1:
+			techLevel = "Agriculture";
+			break;
+		case 2:
+			techLevel = "Medieval";
+			break;
+		case 3:
+			techLevel = "Renaissance";
+			break;
+		case 4:
+			techLevel = "Early Industrial";
+			break;
+		case 5:
+			techLevel = "Industrial";
+			break;
+		case 6:
+			techLevel = "Post-Industrial";
+			break;
+		case 7:
+			techLevel = "Hi-Tech";
+			break;
+		default:
+			techLevel = "undefined";
+			break;
 		}
 	}
-	
+
 	/**
 	 * Setter method for inventory of planet
-	 * @param inventory to set the planetInventory to
+	 * 
+	 * @param inventory
+	 *            to set the planetInventory to
 	 */
-	public void setInventory(PlanetInventory inventory)
-	{
+	public void setInventory(PlanetInventory inventory) {
 		this.inventory = inventory;
 	}
+
 	/**
 	 * getter for planet inventory
+	 * 
 	 * @return the inventory of the planet
 	 */
-	public PlanetInventory getInventory()
-	{
+	public PlanetInventory getInventory() {
 		return inventory;
 	}
 
@@ -94,8 +105,7 @@ public class Planet implements Serializable
 	 * 
 	 * @return the contract the planet has available
 	 */
-	public Contract getContract()
-	{
+	public Contract getContract() {
 		return contract;
 	}
 
@@ -103,8 +113,7 @@ public class Planet implements Serializable
 	 * 
 	 * @return name of the planet
 	 */
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
 
@@ -112,23 +121,15 @@ public class Planet implements Serializable
 	 * 
 	 * @return string tech level
 	 */
-	public String getTechLevel()
-	{
+	public String getTechLevel() {
 		return techLevel;
 	}
-
-/*	public void createContract()
- 	{
-		// randomly generate contract here
-	}
-*/
 
 	/**
 	 * 
 	 * @return coordinates of planet
 	 */
-	public int[] getCoordinate()
-	{
+	public int[] getCoordinate() {
 		return coordinate;
 	}
 
@@ -136,26 +137,32 @@ public class Planet implements Serializable
 	 * 
 	 * @return numerical tech level of planet
 	 */
-	public int getNTechLevel()
-	{
+	public int getNTechLevel() {
 		return nTechLevel;
 	}
-	
+
 	/**
 	 * 
-	 * @param bool - state to set marketBusy to
+	 * @param bool
+	 *            - state to set marketBusy to
 	 */
-	public void setMarketBusy(boolean bool)
-	{
+	public void setMarketBusy(boolean bool) {
 		marketBusy = bool;
 	}
-	
+
 	/**
 	 * 
 	 * @return marketBusy
 	 */
-	public boolean getMarketBusy()
-	{
+	public boolean getMarketBusy() {
 		return marketBusy;
+	}
+	
+	/**
+	 * returns the name of the planet.
+	 * @return name
+	 */
+	public String toString(){
+		return name;
 	}
 }
