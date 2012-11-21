16346c85c4fcec780a0048a7e94d90ba7d667898
Andrew Nelson <werdnanoslen@gmail.com>
2012-11-21 01:42:43 -0500

    replaced literal with Player call; removed unused imports

--- SpaceTrader/src/com/cs2340/spacetrader/PlanetView.java
+++ SpaceTrader/src/com/cs2340/spacetrader/PlanetView.java
@@ -5,7 +5,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
@@ -14,7 +13,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
